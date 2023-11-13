e1ae747803c555b1b2c4041f408cd1aa856a1a2e
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2023-11-13 16:07:01 +0800

    fix: 🐛 Boostrap icon to import

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,4 @@
 
 <style lang="scss">
 @import "./assets/scss/all";
-// 推薦使用 CDN，因為 svg 不一定可以從 node_modules 導出
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css");
 </style>
